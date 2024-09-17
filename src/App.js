f811f514c89d29fc2b8b58580e0d64a358c9b6a4
mhensberg2003 <mhensberg2003@gmail.com>
2024-09-17 20:53:14 +0200

    Initial commit of frontend code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">{message}</h1>
+    </div>
+  );
+}
+
+export default App;
